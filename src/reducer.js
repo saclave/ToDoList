91909ebca5e2307635725910653454eb6631b4f4
Janelle <dominja@oocl.com>
2020-10-27 22:21:28 +0800

    [React][Vea-Janelle] Feature : added action events for delete, change status and add to do

--- src/reducer.js
+++ src/reducer.js
@@ -1,16 +1,18 @@
-const todos = (state = [], action) => {
-    switch(action.type) {
-        case 'ADD_TODO':
-            return [
-                ...state,
-                {
-                    id: action.id,
-                    text: action.text,
-                    completed: false
-                }
-            ];
-        default : return state;    
+import { combineReducers } from "redux";
+
+const actionEvent = (state = [], action) =>{
+    if (action.type === "ADD_TODO") {
+        return [...state, action.payload];
+    } else if (action.type === "DELETE_TODO") {
+        return state.filter(todo => todo.id !== action.payload);
     }
+    else if (action.type === "CHANGE_STATUS") {
+        return state.map(todo => todo.id === action.payload ? { ...todo, done: !todo.done } : todo);
+    }
+    return state;
 }
 
-export default todos;
+
+export default combineReducers({
+  actionEvent
+});
