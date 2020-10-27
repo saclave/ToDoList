21fd68c2ff0e9e384a38b5c728bbe07e05f44c9a
Janelle <dominja@oocl.com>
2020-10-27 22:06:00 +0800

    [React][Vea-Janelle] Refactor : called Todolist component only

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import './App.css';
-import TodoGenerator from './components/TodoGenerator';
 import TodoList from './components/TodoList';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <TodoGenerator />
-        <TodoList />
+        <TodoList/>
       </header>
     </div>
   );
