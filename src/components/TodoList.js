52750c67b848cc63591a4cb86f678862b0b62657
Janelle <dominja@oocl.com>
2020-10-27 22:29:28 +0800

    [React][Vea-Janelle] Feature : added resource container in todo list

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
 import TodoGeneratorContainer from '../containers/TodoGeneratorContainer';
+import TodoResourceContainer from '../containers/TodoResourceContainer';
 
 class TodoList extends Component {
-  render() {
-    return (
-      <div>
-        <h1 id="appName">TodoList</h1>
-        <TodoGeneratorContainer/>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <h1 id="appName">TodoList</h1>
+                <TodoGeneratorContainer />
+                <TodoResourceContainer />
+            </div>
+        );
+    }
 }
 
 export default TodoList;
