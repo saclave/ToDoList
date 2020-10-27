a8ecadd4bceae82b95a29a16fbfd97865a81f001
Janelle <dominja@oocl.com>
2020-10-27 20:38:04 +0800

    [React][Vea-Janelle] Feature: Added action for adding item

--- src/action.js
+++ src/action.js
@@ -1,7 +1,4 @@
-let nextTodoId = 0;
 
-export const addTodo = text => ({
-    type: 'ADD_TODO',
-    id: nextTodoId++,
-    text
-});
+export const addItem = (item) => {
+    return { type: "ADD_ITEM", payload: item };
+}
