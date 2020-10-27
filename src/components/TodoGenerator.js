f626018353234702e25eeb958f4b71556fc22cf6
Janelle <dominja@oocl.com>
2020-10-27 22:19:22 +0800

    [React][Vea-Janelle] Feature : added actions for generation todo list

--- src/components/TodoGenerator.js
+++ src/components/TodoGenerator.js
@@ -1,26 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
-const TodoGenerator = () => {
-    let input;
+class TodoGenerator extends Component {
 
-    const addTask = (e) => {
-        e.preventDefault();
+    constructor(props) {
+        super(props)
 
-        if(!input.value.trim()) return;
-
-        console.log(input.value);
+        this.state = {
+            string: ""
+        };
 
     }
+
+    handleChange =({ target }) => {
+        this.setState({
+          [target.name]: target.value
+        });
+      }
+
+  onSubmit = (event) => {
+    event.preventDefault();
+    const text = this.state.string;
+    const id = uuidv4();
+    const todo = { id, text, done: false };
+    this.props.addTodo(todo);
+  }
+
+  render() {
     return (
-        <div>
-            <form onSubmit={addTask}>
-                <input ref={node => input = node}/>
-                <button type="submit">
-                    Add Todo
-                </button>
-            </form>
-        </div>
-    )
+      <div id="todoGenerator">
+            <input type="text" name="string" id="string" value={ this.state.string } 
+            placeholder="Please add todo here"  onChange={ this.handleChange }  />
+            <input type="submit" value="Add Item" onClick={this.onSubmit} />
+      </div>
+    );
+  }
 }
 
 export default TodoGenerator;
